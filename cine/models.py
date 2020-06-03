7307fbb06aa78ed0e928f48c666158d6f09e6045
Ivan Sanchez <ivan.sanchez@correo.unimet.edu.ve>
2020-06-02 21:45:41 -0400

    Changes to be committed: 	commit:   cine/models.py

--- cine/models.py
+++ cine/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 class Peliculas(models.Model):
-    duracion = models.CharField(max_length=200)
-    titulo = models.CharField(max_length = 200)
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    titulo   = models.CharField(max_length=200)
+    duracion = models.DurationField()
+    reparto  = models.TextField()
+    director = models.CharField(max_length=200)
+    genero   = models.CharField(max_length=200)
+    idioma   = models.CharField(max_length=200)
+    imagen   = models.ImageField(upload_to = os.path.join(BASE_DIR))
+    modalidad = models.IntegerField(default = 0)
+    cartelera = models.BooleanField(default = False)
+
+    def titulo(self):
+        return self.titulo
+    def duracion(self):
+        return self.duracion
+    def idioma(self):
+        return self.idioma
 
 
 
